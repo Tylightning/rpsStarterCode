0f453ff43504de15cc4bed82b3e119fe7e6c2939
tylightning <Tirike.nyc@gmail.com>
2016-12-21 23:09:42 +0000

    rps

--- starter_code/script.js
+++ starter_code/script.js
@@ -9,9 +9,9 @@ var generateComputerChoice = function() {
     // Task 1, Step 3: Use this randomly generated number to pull a value from the
     // array (eg myArray[randomNum])
     rps[generater];
-
+var i = rps[generator];
     // Task 1, Step 4: return this new value
-    return rps[generater];
+    return i;
 
 };
 
@@ -22,12 +22,30 @@ var pickWinner = function(userChoice, computerChoice) {
     console.log("computer choice: " + computerChoice);
     // Task 2, Step 1: Create an "if/else if/else" statement that compares the
     // userChoice and computerChoice under any possible game outcome.
-
+    //   var winner = if (y<x,y>z,x=rock,y=paper,z=scissors ){
+           
+    //   }
+    if (userChoice == computerChoice){
+        console.log("draw");
+    }
+    if (userChoice == "rock" && computerChoice == "paper"){
+        console.log("computerwins")
+    }
+    if (userChoice == "rock" && computerChoice == "scissors"){
+        console.log("userwins");
+    }
+    if (userChoice == "paper" && computerChoice == "rock"){
+        console.log("userwins");
+    }
+    if (userChoice == "paper" && computerChoice == "scissors"){
+        console.log("computerwins");
+    }
+    if (userChoice)
     // Task 2, Step 2: Depending on who is the winner of the game console.log
     // either "user wins", "computer wins" or "draw"
 
     // Task 4: Show `computerChoice` in HTML after the words "Computer's choice:"
-
+    $('#computer-choice').html(computerChoice);
     // Task 5: Show the winner in HTML after the word "Winner:"
 };
 
@@ -41,11 +59,14 @@ $(document).ready(function() {
 
     // This line sets `userChoice` variable to 'rock'. This value can be changed
     // manually when testing in the console.
-    var userChoice = "rock";
+    var userChoice = null;
     // Task 3: To be completed AFTER this game functions in the console.
     // Set `userChoice` to "null" and create click handlers that changes the
     // value of userChoice based on the item the user clicks on the HTML page.
-
+    $(".choice").click(function(e){
+        var element = e.target;
+        console.log(element.id);
+    })
     // This line calls the `pickWinner` function with the `userChoice` variable
     // and the `computerChoice` variable.
     pickWinner(userChoice, computerChoice);
